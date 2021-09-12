3dd4e0a87dc93858d5b2d55bae6c03256ff9daf5
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-09-12 00:27:45 -0500

    se elimina archivos

--- src/app/models/alumno.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Alumno } from './alumno.model';
-
-describe('Alumno', () => {
-  it('should create an instance', () => {
-    expect(new Alumno()).toBeTruthy();
-  });
-});
--- src/app/models/alumno.model.ts
+++
@@ -1,7 +0,0 @@
-export class Alumno {
-    idAlumno?:number;
-    nombre?:string;
-    dni?:string;
-    correo?:string;
-    fechaNacimiento?:Date
-}
